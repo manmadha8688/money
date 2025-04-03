856f498bc0a59adcd7e2abcd2c6c5a75c79b81ba
Friendlysiva143 <friendlysiva143@gmail.com>
2025-04-03 17:05:51 +0530

    added

---
+++ dashboard/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def dashboard(request):
+    return render(request,'dashboard.html')
+def borrowers(request):
+    return render(request,'borrowers.html')
